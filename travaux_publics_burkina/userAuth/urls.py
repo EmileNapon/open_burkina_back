230c29924b9039601d1860a8ba2310d4353c5806
EmileNapon <emilenapon0@gmail.com>
2025-02-21 15:19:19 +0000

    first commit

--- travaux_publics_burkina/userAuth/urls.py
+++ travaux_publics_burkina/userAuth/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CustomTokenObtainPairView, RegisterView, UserDetailView, list_users
 
 urlpatterns = [
-    path('public-project/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('register/', RegisterView.as_view(), name='register_user'),
-    path('user/', UserDetailView.as_view(), name='user_detail'),
-    path('users/', list_users, name='list_users'),
+    path('plateforme-integre/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('plateforme-integre/register/', RegisterView.as_view(), name='register_user'),
+    path('plateforme-integre/user/', UserDetailView.as_view(), name='user_detail'),
+    path('plateforme-integre/users/', list_users, name='list_users'),
 ]
